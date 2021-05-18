513f0b1d1b0a36b2cd9d15ea9815ca2d04ed2591
bsao17 <declic62@gmail.com>
2021-05-18 18:32:40 +0200

    Add Tilt.js

--- src/components/BodyFolio.js
+++ src/components/BodyFolio.js
@@ -1,53 +1,83 @@
 import React from "react";
+import Tilt from "react-tilt";
 
 export default function BodyFolio() {
   return (
     <div className="wrapper">
-      <div class="agency">
-        <img src="https://res.cloudinary.com/bsao17/image/upload/v1621271218/devwebdino/agency_mo6swl.jpg" alt="" />
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet sit sint
-        repellat ullam earum necessitatibus repudiandae harum laboriosam et
-        distinctio perspiciatis, molestiae eaque, nobis quidem quia dicta
-        voluptas ratione, nesciunt ipsa. Voluptate maxime facere dicta quisquam
-        fugit porro eveniet! Nostrum sint harum eos cumque corporis maiores
-        beatae quae dolore. Placeat. lorem50
-      </div>
-      <div class="bikeRenting">
-        <img src="https://res.cloudinary.com/bsao17/image/upload/v1621271227/devwebdino/bikeRenting_e5x9n3.jpg" alt="" />
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet sit sint
-        repellat ullam earum necessitatibus repudiandae harum laboriosam et
-        distinctio perspiciatis, molestiae eaque, nobis quidem quia dicta
-        voluptas ratione, nesciunt ipsa. Voluptate maxime facere dicta quisquam
-        fugit porro eveniet! Nostrum sint harum eos cumque corporis maiores
-        beatae quae dolore. Placeat.
-      </div>
-      <div class="forteroche">
-        <img src="https://res.cloudinary.com/bsao17/image/upload/v1621271224/devwebdino/forteroche_kgo8ux.jpg" alt="" />
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet sit sint
-        repellat ullam earum necessitatibus repudiandae harum laboriosam et
-        distinctio perspiciatis, molestiae eaque, nobis quidem quia dicta
-        voluptas ratione, nesciunt ipsa. Voluptate maxime facere dicta quisquam
-        fugit porro eveniet! Nostrum sint harum eos cumque corporis maiores
-        beatae quae dolore. Placeat.
-      </div>
-      <div class="ireki">
-        <img src="https://res.cloudinary.com/bsao17/image/upload/v1621271223/devwebdino/ireki_tssz7m.jpg" alt="" />
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet sit sint
-        repellat ullam earum necessitatibus repudiandae harum laboriosam et
-        distinctio perspiciatis, molestiae eaque, nobis quidem quia dicta
-        voluptas ratione, nesciunt ipsa. Voluptate maxime facere dicta quisquam
-        fugit porro eveniet! Nostrum sint harum eos cumque corporis maiores
-        beatae quae dolore. Placeat.
-      </div>
-      <div class="jmpm">
-        <img src="https://res.cloudinary.com/bsao17/image/upload/v1621271218/devwebdino/jmpm_egbkof.jpg" alt="" />
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet sit sint
-        repellat ullam earum necessitatibus repudiandae harum laboriosam et
-        distinctio perspiciatis, molestiae eaque, nobis quidem quia dicta
-        voluptas ratione, nesciunt ipsa. Voluptate maxime facere dicta quisquam
-        fugit porro eveniet! Nostrum sint harum eos cumque corporis maiores
-        beatae quae dolore. Placeat.
-      </div>
+      <Tilt options={{ max : 25 }}>
+        <div class="agency">
+          <img
+            src="https://res.cloudinary.com/bsao17/image/upload/v1621271218/devwebdino/agency_mo6swl.jpg"
+            alt=""
+          />
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet sit
+          sint repellat ullam earum necessitatibus repudiandae harum laboriosam
+          et distinctio perspiciatis, molestiae eaque, nobis quidem quia dicta
+          voluptas ratione, nesciunt ipsa. Voluptate maxime facere dicta
+          quisquam fugit porro eveniet! Nostrum sint harum eos cumque corporis
+          maiores beatae quae dolore. Placeat. lorem50
+        </div>
+      </Tilt>
+
+      <Tilt>
+        <div class="bikeRenting">
+          <img
+            src="https://res.cloudinary.com/bsao17/image/upload/v1621271227/devwebdino/bikeRenting_e5x9n3.jpg"
+            alt=""
+          />
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet sit
+          sint repellat ullam earum necessitatibus repudiandae harum laboriosam
+          et distinctio perspiciatis, molestiae eaque, nobis quidem quia dicta
+          voluptas ratione, nesciunt ipsa. Voluptate maxime facere dicta
+          quisquam fugit porro eveniet! Nostrum sint harum eos cumque corporis
+          maiores beatae quae dolore. Placeat.
+        </div>
+      </Tilt>
+
+      <Tilt>
+        <div class="forteroche">
+          <img
+            src="https://res.cloudinary.com/bsao17/image/upload/v1621271224/devwebdino/forteroche_kgo8ux.jpg"
+            alt=""
+          />
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet sit
+          sint repellat ullam earum necessitatibus repudiandae harum laboriosam
+          et distinctio perspiciatis, molestiae eaque, nobis quidem quia dicta
+          voluptas ratione, nesciunt ipsa. Voluptate maxime facere dicta
+          quisquam fugit porro eveniet! Nostrum sint harum eos cumque corporis
+          maiores beatae quae dolore. Placeat.
+        </div>
+      </Tilt>
+
+      <Tilt>
+        <div class="ireki">
+          <img
+            src="https://res.cloudinary.com/bsao17/image/upload/v1621271223/devwebdino/ireki_tssz7m.jpg"
+            alt=""
+          />
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet sit
+          sint repellat ullam earum necessitatibus repudiandae harum laboriosam
+          et distinctio perspiciatis, molestiae eaque, nobis quidem quia dicta
+          voluptas ratione, nesciunt ipsa. Voluptate maxime facere dicta
+          quisquam fugit porro eveniet! Nostrum sint harum eos cumque corporis
+          maiores beatae quae dolore. Placeat.
+        </div>
+      </Tilt>
+
+      <Tilt>
+        <div class="jmpm">
+          <img
+            src="https://res.cloudinary.com/bsao17/image/upload/v1621271218/devwebdino/jmpm_egbkof.jpg"
+            alt=""
+          />
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet sit
+          sint repellat ullam earum necessitatibus repudiandae harum laboriosam
+          et distinctio perspiciatis, molestiae eaque, nobis quidem quia dicta
+          voluptas ratione, nesciunt ipsa. Voluptate maxime facere dicta
+          quisquam fugit porro eveniet! Nostrum sint harum eos cumque corporis
+          maiores beatae quae dolore. Placeat.
+        </div>
+      </Tilt>
     </div>
   );
 }
