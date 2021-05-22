925fbb8007b528c769a5f7fce2a6a8663754af5d
bsao17 <declic62@gmail.com>
2021-05-22 15:35:17 +0200

    Update dwd

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,17 +1,19 @@
 import React from "react";
-import {Navbar, Nav, NavDropdown, Form, FormControl, Button} from 'react-bootstrap'
+import {NavLink} from "react-router-dom";
+import {Navbar, Nav} from 'react-bootstrap'
 
 export default function Navigation() {
   return (
     <div>
-      <Navbar className="bg-dark text-light d-flex align-items-center justify-content-center flex-wrap">
-        <Navbar.Brand href="/" className="mx-3 text-light">devwebdino.com </Navbar.Brand>
+      <Navbar className="link bg-dark text-light d-flex align-items-center justify-content-center flex-wrap">
+        <Navbar.Brand className="mx-3 text-light">devwebdino.com </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link href="/" className="text-light">Accueil</Nav.Link>
-            <Nav.Link href="/portfolio" className="text-light">Portfolio</Nav.Link>
-            <Nav.Link href="/price" className="text-light">Tarifs</Nav.Link>
+            <NavLink to="/" className="link text-light mx-2">Accueil</NavLink>
+            <NavLink to="/portfolio" className="link text-light mx-3">Portfolio</NavLink>
+            <NavLink to="/price" className="link text-light mx-3">Tarifs</NavLink>
+            <Nav><a className="link text-light mx-3" href="http://agence-web.devwebdino.com/" target={"_blank"}>Blog</a></Nav>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
